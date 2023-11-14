f30ea42a7890421d90d8ecfed833583b22c6d90c
Filizanka <104316839+Filizanka1337@users.noreply.github.com>
2023-11-14 15:34:21 +0100

    files
    files :D

---
+++ data/del.py
@@ -0,0 +1,41 @@
+#importowanie 
+import time, os
+
+#dzialanie 
+
+print("odpalanie auto destrukcji")
+time.sleep(1)
+print("za")
+time.sleep(0.6)
+print("4")
+time.sleep(0.9)
+print("3")
+time.sleep(0.9)
+print("2")
+time.sleep(1.2)
+print("1")
+
+time.sleep(1.3)
+
+print("auto destrukcja")
+
+time.sleep(0.8)
+
+print(".")
+
+time.sleep(0.6)
+
+print("..")
+
+time.sleep(0.9)
+
+print("...")
+
+time.sleep(1.3)
+
+print("jk")
+
+time.sleep(2.2)
+
+#papuga 
+os.system("curl parrot.live")
